120f36b5bbba0fafbb69b2da613f2942c97b612f
Guilherme Medeiros <contato@frenetic.com.br>
2012-09-12 14:15:53 -0300

    Fazendo o settings.py incluir os assets do projeto. Corrigindo a importação dos modulos 'itens' e 'players'

--- djangowars/settings.py
+++ djangowars/settings.py
@@ -1,4 +1,8 @@
+import os.path
+
+
 # Django settings for djangowars project.
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -59,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ( 'assets', os.path.join(os.path.dirname(__file__), 'assets').replace('\\', '/') ), # encontrando a pasta assets
 )
 
 # List of finder classes that know how to find static files in
@@ -122,8 +127,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'players',
-    'itens'
+    'djangowars.players',
+    'djangowars.itens'
 )
 
 # A sample logging configuration. The only tangible logging
