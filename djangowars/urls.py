0fb25f0cd015d3b0eb9bba4f99a73f8a7914ad3c
Guilherme Medeiros <contato@frenetic.com.br>
2012-09-12 16:53:56 -0300

    Corrigindo o arquivo urls.py para exibir poder carregar a views corretamente

--- djangowars/urls.py
+++ djangowars/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+import djangowars.views
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
