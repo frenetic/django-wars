666a75cafc334af7704f410808ff8fd8a34f7502
Guilherme Medeiros <contato@frenetic.com.br>
2012-09-12 01:31:11 -0300

    Inserindo as apps Players e Itens no settings.py

--- djangowars/settings.py
+++ djangowars/settings.py
@@ -122,6 +122,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'players',
+    'itens'
 )
 
 # A sample logging configuration. The only tangible logging
