1a4f9196ff2c972312b8d956e969453bc9076eb2
Guilherme Medeiros <contato@frenetic.com.br>
2012-09-13 16:17:01 -0300

    Criando um simples formulário de registro de usuário

--- djangowars/urls.py
+++ djangowars/urls.py
@@ -8,6 +8,7 @@ import djangowars.views
 
 urlpatterns = patterns('',
                        url(r'^$', djangowars.views.index),
+                       url(r'^registrar/$', djangowars.views.registrar), # pagina de cadastro
     # Examples:
     # url(r'^$', 'djangowars.views.home', name='home'),
     # url(r'^djangowars/', include('djangowars.foo.urls')),
