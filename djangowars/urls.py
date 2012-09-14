cefaaaf9d76a4b8b5129fe8269e2cf6af01cd4e6
Guilherme Medeiros <contato@frenetic.com.br>
2012-09-14 17:06:56 -0300

    Ativando o Django-admin no projeto

--- djangowars/urls.py
+++ djangowars/urls.py
@@ -3,20 +3,17 @@ from django.conf.urls import patterns, include, url
 import djangowars.views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', djangowars.views.index),
                        url(r'^registrar/$', djangowars.views.registrar), # pagina de cadastro
                        url(r'^login/$', djangowars.views.logar), # pagina de login
-    # Examples:
-    # url(r'^$', 'djangowars.views.home', name='home'),
-    # url(r'^djangowars/', include('djangowars.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
