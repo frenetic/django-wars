512763f68256e8273afb5a2f512d9256ddb0573d
Guilherme Medeiros <contato@frenetic.com.br>
2012-09-14 16:19:30 -0300

    Criando os modelos das armas e armaduras

--- djangowars/itens/models.py
+++ djangowars/itens/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Arma(models.Model):
+    poder = models.PositiveSmallIntegerField()
+    compra = models.FloatField() # valor de compra
+    venda = models.FloatField() # valor de venda
+    secreta = models.BooleanField(default=False) #se a arma eh secreta ou disponivel na loja
+    
+    imagem = models.ImageField(upload_to="armas") #imagem do item
+
+
+class Armadura(models.Model):
+    poder = models.PositiveSmallIntegerField()
+    compra = models.FloatField() # valor de compra
+    venda = models.FloatField() # valor de venda
+    secreta = models.BooleanField(default=False) #se a armadura eh secreta ou disponivel na loja
+    
+    imagem = models.ImageField(upload_to="armaduras") #imagem do item
