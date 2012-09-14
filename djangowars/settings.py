6ad4ebba3434a137521abb92985b5ac0ae69687c
Guilherme Medeiros <contato@frenetic.com.br>
2012-09-14 16:19:44 -0300

    Configurando o MEDIA_ROOT para poder fazer upload de imagens

--- djangowars/settings.py
+++ djangowars/settings.py
@@ -52,7 +52,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'assets/itens').replace('\\', '/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
