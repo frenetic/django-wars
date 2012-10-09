bfa0577e24accd690aa812c395f499c75266be55
Guilherme Medeiros <contato@frenetic.com.br>
2012-10-09 10:02:40 -0300

    Dando uma leve organizada no arquivo urls.py

--- djangowars/urls.py
+++ djangowars/urls.py
@@ -15,12 +15,11 @@ urlpatterns = patterns('',
                        url(r'^crimes/cometer/1/$', djangowars.views.cometer_crime1),
                        url(r'^crimes/cometer/2/$', djangowars.views.cometer_crime2),
 
-
                        url(r'^loja/$', djangowars.views.loja), # pagina de loja
                        url(r'^loja/comprar/armadura/(\d+)/$', djangowars.views.comprar_armadura), # pagina de loja
                        url(r'^loja/vender/armadura/(\d+)/$', djangowars.views.vender_armadura), # pagina de loja
-                       #url(r'^compra/arma/(\d+)/$', djangowars.views.compra_arma), # pagina de loja
-                       #url(r'^venda/arma/(\d+)/$', djangowars.views.venda_arma), # pagina de loja
+                       url(r'^loja/comprar/arma/(\d+)/$', djangowars.views.comprar_arma), # pagina de loja
+                       url(r'^loja/vender/arma/(\d+)/$', djangowars.views.vender_arma), # pagina de loja
                        
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
