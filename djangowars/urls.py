4cabc455094af0821758700f8a8475d31ddfb68b
Guilherme Medeiros <contato@frenetic.com.br>
2012-10-10 15:36:31 -0300

    Adicionando o link para o inventário no urls.py

--- djangowars/urls.py
+++ djangowars/urls.py
@@ -10,16 +10,19 @@ urlpatterns = patterns('',
                        url(r'^$', djangowars.views.index),
                        url(r'^registrar/$', djangowars.views.registrar), # pagina de cadastro
                        url(r'^login/$', djangowars.views.logar), # pagina de login
-
+                       
                        url(r'^crimes/$', djangowars.views.crimes),
                        url(r'^crimes/cometer/1/$', djangowars.views.cometer_crime1),
                        url(r'^crimes/cometer/2/$', djangowars.views.cometer_crime2),
-
+                       
                        url(r'^loja/$', djangowars.views.loja), # pagina de loja
                        url(r'^loja/comprar/armadura/(\d+)/$', djangowars.views.comprar_armadura), # pagina de loja
                        url(r'^loja/vender/armadura/(\d+)/$', djangowars.views.vender_armadura), # pagina de loja
                        url(r'^loja/comprar/arma/(\d+)/$', djangowars.views.comprar_arma), # pagina de loja
                        url(r'^loja/vender/arma/(\d+)/$', djangowars.views.vender_arma), # pagina de loja
+                       
+                       url(r'^inventario/$', djangowars.views.inventario),
+                       
                        
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
