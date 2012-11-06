80e57da1ed389c5208dc971d77dc48b5efc430a6
Guilherme Medeiros <contato@frenetic.com.br>
2012-11-06 12:00:02 -0300

    Atualizando o arquivo urls.py

--- djangowars/urls.py
+++ djangowars/urls.py
@@ -7,23 +7,24 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', djangowars.views.index),
-                       url(r'^registrar/$', djangowars.views.registrar), # pagina de cadastro
-                       url(r'^login/$', djangowars.views.logar), # pagina de login
+                       url(r'^$', djangowars.views.site.index),
                        
-                       url(r'^crimes/$', djangowars.views.crimes),
-                       url(r'^crimes/cometer/1/$', djangowars.views.cometer_crime1),
-                       url(r'^crimes/cometer/2/$', djangowars.views.cometer_crime2),
+                       url(r'^registrar/$', djangowars.views.player.registrar), # pagina de cadastro
+                       url(r'^login/$', djangowars.views.player.logar), # pagina de login
                        
-                       url(r'^loja/$', djangowars.views.loja), # pagina de loja
-                       url(r'^loja/comprar/armadura/(\d+)/$', djangowars.views.comprar_armadura), # pagina de loja
-                       url(r'^loja/vender/armadura/(\d+)/$', djangowars.views.vender_armadura), # pagina de loja
-                       url(r'^loja/comprar/arma/(\d+)/$', djangowars.views.comprar_arma), # pagina de loja
-                       url(r'^loja/vender/arma/(\d+)/$', djangowars.views.vender_arma), # pagina de loja
+                       url(r'^crimes/$', djangowars.views.crimes.crimes),
+                       url(r'^crimes/cometer/1/$', djangowars.views.crimes.cometer_crime1),
+                       url(r'^crimes/cometer/2/$', djangowars.views.crimes.cometer_crime2),
                        
-                       url(r'^inventario/$', djangowars.views.inventario),
-                       url(r'^inventario/equipar/armadura/(\d+)/$', djangowars.views.equipar_armadura),
-                       url(r'^inventario/equipar/arma/(\d+)/$', djangowars.views.equipar_arma),
+                       url(r'^loja/$', djangowars.views.loja.loja), # pagina de loja
+                       url(r'^loja/comprar/armadura/(\d+)/$', djangowars.views.loja.comprar_armadura), # pagina de loja
+                       url(r'^loja/vender/armadura/(\d+)/$', djangowars.views.loja.vender_armadura), # pagina de loja
+                       url(r'^loja/comprar/arma/(\d+)/$', djangowars.views.loja.comprar_arma), # pagina de loja
+                       url(r'^loja/vender/arma/(\d+)/$', djangowars.views.loja.vender_arma), # pagina de loja
+                       
+                       url(r'^inventario/$', djangowars.views.inventario.inventario),
+                       url(r'^inventario/equipar/armadura/(\d+)/$', djangowars.views.inventario.equipar_armadura),
+                       url(r'^inventario/equipar/arma/(\d+)/$', djangowars.views.inventario.equipar_arma),
                        
                        
                        # Uncomment the next line to enable the admin:
