12493a700b26e0c07e663408729f2f0d9d60b0fa
Guilherme Medeiros <contato@frenetic.com.br>
2012-11-06 17:08:27 -0300

    Dando nomes as urls

--- djangowars/urls.py
+++ djangowars/urls.py
@@ -14,20 +14,20 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^$', djangowars.views.site.index),
                        
-                       url(r'^registrar/$', djangowars.views.player.registrar), # pagina de cadastro
-                       url(r'^login/$', djangowars.views.player.logar), # pagina de login
+                       url(r'^registrar/$', djangowars.views.player.registrar, name='pagina_de_registro'), # pagina de cadastro
+                       url(r'^login/$', djangowars.views.player.logar, name='pagina_de_login'), # pagina de login
                        
-                       url(r'^crimes/$', djangowars.views.crimes.crimes),
+                       url(r'^crimes/$', djangowars.views.crimes.crimes, name='pagina_de_crimes'),
                        url(r'^crimes/cometer/1/$', djangowars.views.crimes.cometer_crime1),
                        url(r'^crimes/cometer/2/$', djangowars.views.crimes.cometer_crime2),
                        
-                       url(r'^loja/$', djangowars.views.loja.loja), # pagina de loja
+                       url(r'^loja/$', djangowars.views.loja.loja, name='pagina_da_loja'), # pagina de loja
                        url(r'^loja/comprar/armadura/(\d+)/$', djangowars.views.loja.comprar_armadura), # pagina de loja
                        url(r'^loja/vender/armadura/(\d+)/$', djangowars.views.loja.vender_armadura), # pagina de loja
                        url(r'^loja/comprar/arma/(\d+)/$', djangowars.views.loja.comprar_arma), # pagina de loja
                        url(r'^loja/vender/arma/(\d+)/$', djangowars.views.loja.vender_arma), # pagina de loja
                        
-                       url(r'^inventario/$', djangowars.views.inventario.inventario),
+                       url(r'^inventario/$', djangowars.views.inventario.inventario, name='pagina_do_inventario'),
                        url(r'^inventario/equipar/armadura/(\d+)/$', djangowars.views.inventario.equipar_armadura),
                        url(r'^inventario/equipar/arma/(\d+)/$', djangowars.views.inventario.equipar_arma),
                        
