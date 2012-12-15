c02169de4acd99182170af73b74b3cc49656694e
Guilherme Medeiros <contato@frenetic.com.br>
2012-12-15 00:55:36 -0300

    organizando alguns crimes

--- djangowars/views/crimes.py
+++ djangowars/views/crimes.py
@@ -17,6 +17,7 @@ def crimes(request):
     player.refresh()
     player.save()
     
+    
     return render_to_response("crimes.html", {"player": request.user.get_profile(),
                                               "vida": request.user.get_profile().vida * 10}) # para exibir o total de vida do usuario
 
@@ -59,6 +60,55 @@ def cometer_crime2(request):
 
     #adiciona experiencia ao jogador
     player.experiencia = player.experiencia + 10
+    
+    #verifica se subiu de nivel
+    player.level_up()
+
+
+    player.save()
+    return redirect(crimes)
+
+
+# cometendo o crime de nivel 3
+def cometer_crime3(request):
+    if not request.user.is_authenticated():
+        return redirect('pagina_de_login')
+
+    player = request.user.get_profile()
+
+    if player.energia_atual < 4 or player.nivel < 3 or not player.arma_ativa:
+        return redirect(crimes)
+
+    player.carteira = player.carteira + (30 * randint(0, player.ataque + player.arma_ativa.poder))
+    player.energia_atual = player.energia_atual - 4
+
+    #adiciona experiencia ao jogador
+    player.experiencia = player.experiencia + 20
+    
+    #verifica se subiu de nivel
+    player.level_up()
+
+
+    player.save()
+    return redirect(crimes)
+
+
+# cometendo o crime de nivel 4
+def cometer_crime4(request):
+    if not request.user.is_authenticated():
+        return redirect('pagina_de_login')
+
+    player = request.user.get_profile()
+
+    if player.energia_atual < 10 or player.nivel < 5 or not player.arma_ativa or not player.armadura_ativa:
+        return redirect(crimes)
+    
+    
+    player.carteira = player.carteira + (40 * randint(0, player.ataque + player.arma_ativa.poder))
+    player.energia_atual = player.energia_atual - 10
+
+    #adiciona experiencia ao jogador
+    player.experiencia = player.experiencia + 50
     
     #verifica se subiu de nivel
     player.level_up()
